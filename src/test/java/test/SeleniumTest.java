a2f2e7bc938a47d530a5e5e4dd1c180a0959cd59
daniel.tescu <Daniel.Tescu@4finance.com>
2018-04-25 14:08:03 +0300

    Firefox and Chrome example

---
+++ src/test/java/test/SeleniumTest.java
@@ -0,0 +1,48 @@
+package test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SeleniumTest {
+    private static WebDriver driver;
+    
+    @Test
+    public void testChromeDriver() {
+        System.setProperty("webdriver.gecko.driver", "./src/test/java/resources/geckodriver.exe");
+        driver = new FirefoxDriver();
+        driver.get("http://www.google.com");
+
+        WebDriverWait wait = new WebDriverWait(driver,5);
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.name("q"))));
+
+        WebElement input = driver.findElement(By.name("q"));
+        input.sendKeys("selenium");
+        input.sendKeys(Keys.ENTER);
+
+        driver.quit();
+    }
+    @Test
+    public void testFirefoxDriver() {
+
+        System.setProperty("webdriver.chrome.driver", "./src/test/java/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.youtube.com/watch?v=hvCALPmg9NE");
+        
+        WebDriverWait waitElement = new WebDriverWait(driver,15);
+        waitElement.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("#container > h1 > yt-formatted-string"))));
+                
+        WebElement element = driver.findElement(By.cssSelector("#container > h1 > yt-formatted-string"));
+        System.out.println(element.getText());
+        Assert.assertEquals("Costi Ionita - Cate nopti am plans [HQ]",element.getText());
+        
+        driver.quit();
+    }
+}
